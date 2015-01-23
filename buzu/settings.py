e3684fded67351afcac663f6c403c1cdb363b55e
Diego Leite <diegojleite@gmail.com>
2015-01-23 00:37:06 -0300

    Start a scrapy crawler

---
+++ buzu/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for buzu project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'buzu'
+
+SPIDER_MODULES = ['buzu.spiders']
+NEWSPIDER_MODULE = 'buzu.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'buzu (+http://www.yourdomain.com)'
